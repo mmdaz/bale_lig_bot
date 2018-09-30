716589c1d9ea810681802cbb819ac813ee4d75af
Mohammad Azhdari <mohammad.azhdari.22@gmail.com>
2018-09-30 13:58:50 +0330

    delay update

--- Bot/lig_bot_config.py
+++ Bot/lig_bot_config.py
@@ -7,6 +7,6 @@ class LigBotConfig:
     db_name = os.environ.get('POSTGRES_DB', None) or "DametGarmLig"
     db_user = os.environ.get('POSTGRES_USER', None) or "muhammad"
     db_password = os.environ.get('POSTGRES_PASSWORD', None) or "1540487768"
-    report_delay = int(os.environ.get('REPORT_DELAY', None) or 8)
+    report_delay = int(os.environ.get('REPORT_DELAY', None) or 15)
     bot_token = os.environ.get('BOT_TOKEN', None) or "4da1a22c3bd8f29afcc59fdcc82721c901134f1a"
 
