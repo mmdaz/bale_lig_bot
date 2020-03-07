64f4507c7d1ab641b83d512c80a3ae3a61c80c77
Muhammad Azhdari <35610039+mmdaz@users.noreply.github.com>
2020-03-07 23:17:53 +0330

    Update lig_bot_config.py

--- Bot/lig_bot_config.py
+++ Bot/lig_bot_config.py
@@ -7,8 +7,8 @@ class LigBotConfig:
     admin_user_id = int(os.environ.get('ADMIN_USER_ID', None) or 1314892980)
     db_name = os.environ.get('POSTGRES_DB', None) or "DametGarmLig"
     db_user = os.environ.get('POSTGRES_USER', None) or "muhammad"
-    db_password = os.environ.get('POSTGRES_PASSWORD', None) or "1540487768"
+    db_password = os.environ.get('POSTGRES_PASSWORD', None) or ""
     report_delay1 = int(os.environ.get('REPORT_DELAY1', None) or 15)
     report_delay2 = int(os.environ.get('REPORT_DELAY2', None) or 10)
-    bot_token = os.environ.get('BOT_TOKEN', None) or "4da1a22c3bd8f29afcc59fdcc82721c901134f1a"
+    bot_token = os.environ.get('BOT_TOKEN', None) or ""
     db_host = os.environ.get('DB_HOST', None) or "localhost"
